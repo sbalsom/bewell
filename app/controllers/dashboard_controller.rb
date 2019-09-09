00ca80f939c3800634ba2a462cc5522a253f7647
Sadie Balsom <saralogsin@protonmail.com>
2019-09-09 17:46:39 +0200

    updated dashboard design to include upcoming appointments

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,14 +12,10 @@ class DashboardController < ApplicationController
       @user_therapist = nil
     end
     @bookings = current_user.bookings
-    @next_booking = @bookings.where("starts_at > ? and status <> ?", Date.today, 3)
+    @next_bookings = @bookings.where("starts_at > ? or status <> ?", Date.today, 3)
     @today_booking = Booking
     .where(["starts_at = ? and status <> ?", Date.today, 3])
     .last
-    # this bit of code is supposed to display the next booking you book in the future, if none is scheduled for today. but, it doesn't work reliably
-    # if @today_booking.blank?
-    #   @today_booking = @next_booking.last
-    # end
     past_bookings = @bookings.where(["starts_at < ? or status = ? ", Date.today, 3])
     past_bookings = past_bookings.order(starts_at: :desc)
     @past_bookings = past_bookings.order(updated_at: :desc)
